6c23248a43f80114c7bc95e3f4e9df1b73cd0218
Kuein <kuein@outlook.com>
2020-02-10 18:07:14 +0100

    .cargo

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn my_handler(e: CustomEvent, c: Context) -> Result<CustomOutput, HandlerError> {
     if e.first_name == "" {
-        // correct error handling
-        panic!("Empty first name in request {}", c.aws_request_id);
+        Ok(CustomOutput {
+            message: format!("Empty first name in request {}", c.aws_request_id),
+        })
+    } else {
+        Ok(CustomOutput {
+            message: format!("Hello, {}!", e.first_name),
+        })
     }
-
-    Ok(CustomOutput {
-        message: format!("Hello, {}!", e.first_name),
-    })
 }
