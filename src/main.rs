ec5bfb2bd86bb653b83667940cd55a76d9a129e7
Kuein <kuein@outlook.com>
2020-02-10 18:00:24 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use serde_derive::{Deserialize, Serialize};
+use std::error::Error;
+
+#[derive(Deserialize)]
+struct CustomEvent {
+    #[serde(rename = "firstName")]
+    first_name: String,
+}
+
+#[derive(Serialize)]
+struct CustomOutput {
+    message: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    lambda!(my_handler);
+    Ok(())
+}
+
+fn my_handler(e: CustomEvent, c: Context) -> Result<CustomOutput, HandlerError> {
+    if e.first_name == "" {
+        // correct error handling
+        panic!("Empty first name in request {}", c.aws_request_id);
+    }
+
+    Ok(CustomOutput {
+        message: format!("Hello, {}!", e.first_name),
+    })
+}
